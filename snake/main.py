934bfa85c1c6e3962f66dc5631f81295745ed7d1
Kelly Chang <kachang@justworks.com>
2024-09-17 00:01:21 -0400

    fix snake

--- snake/main.py
+++ snake/main.py
@@ -1,4 +1,4 @@
-from M5 import BtnPWR, BtnB, Widgets
+from M5 import *
 from time import ticks_cpu
 from machine import Timer
 from random import randint, seed
@@ -8,7 +8,10 @@ class Food:
     pos = [0, 0]
     def generate():
         Food.pos = [randint(2, 157), randint(2, 77)]
-        Food.pos = [Food.pos[0] if Food.pos[0] %2 == 0 else Food.pos[0] - 1, Food.pos[1] if Food.pos[1] % 2 == 0 else Food.pos[1] - 1]
+        Food.pos = [
+            Food.pos[0] if Food.pos[0] %2 == 0 else Food.pos[0] - 1,
+            Food.pos[1] if Food.pos[1] % 2 == 0 else Food.pos[1] - 1,
+        ]
         Food.checkPosition()
         
     def checkPosition():
@@ -17,8 +20,8 @@ class Food:
  
 class Snake:
     move_r = [2, 0]
-    move_l = [-2,0]
-    move_u = [0,-2]
+    move_l = [-2, 0]
+    move_u = [0, -2]
     move_d = [0, 2]
     moves = [move_u, move_r, move_d, move_l]
     dr = 1 #direction
@@ -28,7 +31,7 @@ class Snake:
     notail = [2,2]
     isDead = False
     def isAlive(head):
-        return head[0] > 0 and head[0] < 158 and head[1] > 0 and head[1] < 78 and Snake.tail.count(head) == 0
+        return head[0] > 0 and head[0] < 238 and head[1] > 0 and head[1] < 133 and Snake.tail.count(head) == 0
     
     def run():
         seed(ticks_cpu())
@@ -36,14 +39,11 @@ class Snake:
         Snake.timer.init(period=Snake.speed, mode=Timer.PERIODIC, callback=Snake.move)
         
     def dirChange():
-        if BtnB.isPressed():
+        if BtnA.isPressed():
             Snake.dr += 1
         if BtnPWR.isPressed():
             Snake.dr -= 1
-        if Snake.dr > 3:
-            Snake.dr = 0
-        if Snake.dr < 0:
-            Snake.dr = 3
+        Snake.dr %= 4
             
     def reset():
         Snake.isDead = False
@@ -57,9 +57,9 @@ class Snake:
         Snake.timer.init(period=Snake.speed, mode=Timer.PERIODIC, callback=Snake.move)
         
     def move(tmr=None):
+        print("hi")
         head = Snake.tail[0].copy()
         Snake.dirChange()
-        #Snake.basicAIMove(head) #uncomment this line if you want AI to play Snake
         head[0] += Snake.moves[Snake.dr][0]
         head[1] += Snake.moves[Snake.dr][1]
         if [head].count(Food.pos) == 0:
@@ -69,29 +69,21 @@ class Snake:
             Snake.speedUp()
         Snake.isDead = not Snake.isAlive(head)
         Snake.tail.insert(0, head)
-        
-    def basicAIMove(head):
-        if head[0] == Food.pos[0]:
-            Snake.dr = 2 if Food.pos[1] > head[1] and Snake.dr != 0 else 0 if Snake.dr !=2 else Snake.dr
-        if head[1] == Food.pos[1]:
-            Snake.dr = 1 if Food.pos[0] > head[0] and Snake.dr != 3 else 3 if Snake.dr !=1 else Snake.dr
-        if (head[0] > 155 and Snake.dr == 1) or (head[0] < 3 and Snake.dr == 3):
-            print(head[0])
-            Snake.dr = 0 if head[1] > Food.pos[1] else 2
-        if (head[1] > 75 and Snake.dr == 2) or (head[1] < 4 and Snake.dr == 0):
-            Snake.dr = 3 if head[0] > Food.pos[0] else 1
+
 
 class Game:
     def drawBorders():
-        rect0 = Widgets.Rectangle(0, 0, 160, 80)
+        rect0 = Widgets.Rectangle(0, 0, 240, 135)
         
     def prepareField():
+        M5.begin()
         Snake.run()
         Widgets.fillScreen(0x000000)
         Game.drawBorders()
         Food.generate()
         
     def loop():
+        M5.update()
         # render snake
         for tailpart in Snake.tail:
             Widgets.Rectangle(tailpart[0], tailpart[1], 2, 2)
@@ -118,4 +110,6 @@ class Game:
 
 Game.prepareField()
 while True:
-    Game.loop() or Game.over()
+    status = Game.loop()
+    if not status:
+      Game.over()
