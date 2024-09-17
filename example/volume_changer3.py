61db90d02dbefcf706624cc62b770a4a697ff77f
Kelly Chang <kachang@justworks.com>
2024-09-16 22:27:32 -0400

    fix typo

--- example/volume_changer3.py
+++ example/volume_changer3.py
@@ -11,9 +11,9 @@ def setup():
 
 def loop():
     M5.update()
-    x = min(abs(int((Imu.getAttitude()[0] + 180) * 255 / 360)), 255)
-    y = min(abs(int((Imu.getAttitude()[1] + 180) * 255 / 360)), 255)
-    z = min(abs(int((Imu.getAttitude()[2] + 180) * 255 / 360)), 255)
+    x = min(abs(int((Imu.getGryo()[0] + 180) * 255 / 360)), 255)
+    y = min(abs(int((Imu.getGryo()[1] + 180) * 255 / 360)), 255)
+    z = min(abs(int((Imu.getGryo()[2] + 180) * 255 / 360)), 255)
     # print(f"x: {x}, y: {y}, z: {x}")
     Widgets.fillScreen((x << 16) + (y << 8) + z)
     # print(hex((x << 16) + (y << 8) + z))
